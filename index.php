7bc9161f0000541df4f5a40ee3a9b7a90850e548
oranginc <kamiya@oranginc.co.jp>
2013-12-06 20:30:58 +0900

    最新

--- index.php
+++ index.php
@@ -3,73 +3,23 @@
 
 <!-- メインカラム -->
 <div id="contents">
-
+ 
 <!-- カテゴリー一覧 start -->  
 <div id="category_list">
-<h2>街コンを開催地域で探す</h2>
-<p><span class="category_hed">関東</span>   
-| <a href="http://machikon-ex.com/eventlist/?region=東京都">東京都</a>
-| <a href="http://machikon-ex.com/eventlist/?region=神奈川県">神奈川県</a>  
-| <a href="http://machikon-ex.com/eventlist/?region=埼玉県">埼玉県</a>  
-| <a href="http://machikon-ex.com/eventlist/?region=千葉県">千葉県</a>  
-| <a href="http://machikon-ex.com/eventlist/?region=茨城県">茨城県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=栃木県">栃木県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=群馬県">群馬県</a> 
+<?php
+$category_ids = 66; // 親カテゴリーのIDを入れる
+$parent_cat = get_terms( "category", "parent=$category_ids&fields=all&get=all" );
+foreach($parent_cat as $parent_value):
+ ?>
+<p><span class="category_hed"><?php echo $parent_value->name;?></span>   
+<?php
+    $cat_all = get_terms( "category", "parent=$parent_value->term_id&fields=all&get=all" );
+    foreach($cat_all as $value):
+?>
+| <a href="<?php echo get_category_link($value->term_id); ?>"><?php echo $value->name;?></a> 
+<?php endforeach; ?>  
 </p>
-<p><span class="category_hed">北海道・東北</span> 
-| <a href="http://machikon-ex.com/eventlist/?region=北海道">北海道</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=青森県">青森県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=秋田県">秋田県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=岩手県">岩手県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=宮城県">宮城県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=山形県">山形県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=福島県">福島県</a> 
-</p>
-<p><span class="category_hed">北陸・甲信越</span> 
-| <a href="http://machikon-ex.com/eventlist/?region=富山県">富山県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=山梨県">山梨県</a> 
-| <a href="http://machikon-ex.com/eventlist/?region=新潟県">新潟県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=石川県">石川県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=福井県">福井県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=長野県">長野県</a>
-</p>
-<p><span class="category_hed">中部</span>
-| <a href="http://machikon-ex.com/eventlist/?region=愛知県">愛知県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=岐阜県">岐阜県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=三重県">三重県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=静岡県">静岡県</a>
-</p>
-<p><span class="category_hed">関西</span>
-| <a href="http://machikon-ex.com/eventlist/?region=大阪府">大阪府</a>
-| <a href="http://machikon-ex.com/eventlist/?region=京都府">京都府</a>
-| <a href="http://machikon-ex.com/eventlist/?region=奈良県">奈良県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=兵庫県">兵庫県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=和歌山県">和歌山県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=滋賀県">滋賀県</a>  
-</p>
-<p><span class="category_hed">中国</span>
-| <a href="http://machikon-ex.com/eventlist/?region=山口県">山口県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=岡山県">岡山県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=島根県">島根県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=広島県">広島県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=鳥取県">鳥取県</a>
-</p>
-<p><span class="category_hed">四国</span>
-| <a href="http://machikon-ex.com/eventlist/?region=徳島県">徳島県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=愛媛県">愛媛県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=香川県">香川県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=高知県">高知県</a>
-</p>
-<p><span class="category_hed">九州・沖縄</span>
-| <a href="http://machikon-ex.com/eventlist/?region=福岡県">福岡県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=佐賀県">佐賀県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=大分県">大分県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=宮崎県">宮崎県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=熊本県">熊本県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=鹿児島県">鹿児島県</a>
-| <a href="http://machikon-ex.com/eventlist/?region=沖縄県">沖縄県</a>
-</p>
-  
+<?php endforeach; ?>  
 </div>
 <!-- カテゴリー一覧 end -->
 
